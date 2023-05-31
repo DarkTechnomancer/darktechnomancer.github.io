2bf53baace0e294d80e648690a9cca3c758529a3
DarkTechnomancer <DarkTechnomancer2@gmail.com>
2023-05-31 13:24:10 -0400

    Code rewrite
    Messed up uploading them the first time.

--- Part 2: Proto-Batcher/tGrow.js
+++ Part 2: Proto-Batcher/tGrow.js
@@ -1,18 +1,23 @@
 /*
-Workers have been given a minor modification to cut the timestamp down to be a bit more readable.
-More importantly, the weaken script now reports back to the controller when finished running.
+	Very little has changed in the workers. We uncommented a couple of parts to allow for the ping-pong deployment.
+	See the tWeaken.js for full comments.
 */
 
 /** @param {NS} ns */
 export async function main(ns) {
-	const metrics = JSON.parse(ns.args[0]);
-	const delay = metrics.end - metrics.time - Date.now();
+	const job = JSON.parse(ns.args[0]);
+	let delay = job.end - job.time - Date.now();
 	if (delay < 0) {
-		ns.tprint(`ERROR: ${metrics.type} was ${-delay}ms too late.`);
-		ns.tprint(metrics);
-		return;
+		ns.tprint(`WARN: Batch ${job.batch} ${job.type} was ${-delay}ms late. (${job.end})\n`);
+		ns.writePort(ns.pid, -delay);
+		delay = 0;
+	} else {
+		ns.writePort(ns.pid, 0);
 	}
-	await ns.grow(metrics.target, { additionalMsec: delay });
+	await ns.grow(job.target, { additionalMsec: delay });
 	const end = Date.now();
-	ns.tprint(`${metrics.type} finished at ${end.toString().slice(-6)}/${Math.round(metrics.end).toString().slice(-6)}`);
+	ns.atExit(() => {
+		// if (job.report) ns.writePort(job.port, job.type + job.server);
+		ns.tprint(`Batch ${job.batch}: ${job.type} finished at ${end.toString().slice(-6)}/${Math.round(job.end).toString().slice(-6)}\n`);
+	});
 }
--- Part 2: Proto-Batcher/tWeaken.js
+++ Part 2: Proto-Batcher/tWeaken.js
@@ -1,23 +1,27 @@
 /*
-Workers have been given a minor modification to cut the timestamp down to be a bit more readable.
-More importantly, the weaken script now reports back to the controller when finished running.
+	Very little has changed in the workers. We uncommented a couple of parts to allow for the ping-pong deployment.
+	See the tWeaken.js for full comments.
 */
 
 /** @param {NS} ns */
 export async function main(ns) {
-	const metrics = JSON.parse(ns.args[0]);
-	const delay = metrics.end - metrics.time - Date.now();
+	const job = JSON.parse(ns.args[0]);
+
+	let delay = job.end - job.time - Date.now();
 	if (delay < 0) {
-		ns.tprint(`ERROR: ${metrics.type} was ${-delay}ms too late.`);
-		ns.tprint(metrics);
-		return;
+		// We now write back to the controller if jobs are delayed so that it can adjust the other jobs to match.
+		ns.tprint(`WARN: Batch ${job.batch} ${job.type} was ${-delay}ms late. (${job.end})\n`);
+		ns.writePort(ns.pid, -delay);
+		delay = 0;
+	} else {
+		ns.writePort(ns.pid, 0);
 	}
-	await ns.weaken(metrics.target, { additionalMsec: delay });
+	await ns.weaken(job.target, { additionalMsec: delay });
 	const end = Date.now();
 
-	// Write back to the controller when W2 finishes. We pack it into the atExit callback to make sure RAM is cleared.
+	// Write back to let the controller know that we're done. The actual data is currently only used by the prep function.
 	ns.atExit(() => {
-		if (metrics.type === "weaken2") ns.writePort(metrics.port, metrics.type);
-		ns.tprint(`${metrics.type} finished at ${end.toString().slice(-6)}/${Math.round(metrics.end).toString().slice(-6)}`);
+		if (job.report) ns.writePort(job.port, job.type + job.server);
+		ns.tprint(`Batch ${job.batch}: ${job.type} finished at ${end.toString().slice(-6)}/${Math.round(job.end).toString().slice(-6)}\n`);
 	});
 }
